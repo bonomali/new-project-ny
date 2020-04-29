71c238bd39fe7ac99baa7cb9dfe7903a62f925d8
Michelle Scharfstein <mscharfstein@google.com>
2020-04-29 16:03:13 -0400

    Add basic form for entering caller details and call topic. Has placeholder for calling backend to add call reservation.

--- call-me-back-app/web/frontend/src/Routes.jsx
+++ call-me-back-app/web/frontend/src/Routes.jsx
@@ -3,10 +3,12 @@ import {Router} from 'react-router'
 import { Route, Switch } from 'react-router-dom'
 import Home from './components/Home.jsx'
 import history from './history'
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 function Routes() {
     return (
         <Router history={history}>
+            <CssBaseline />
             <div className="container-fluid">
                 <Switch>
                     <Route path='/home' component={Home} />
