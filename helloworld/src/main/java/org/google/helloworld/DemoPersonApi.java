44a805660a5500a68fc077d9b416952432eabc27
Tariq Yusuf <yusuft@google.com>
2020-04-30 11:34:34 -0800

    Make mongodb host/port load dynamically

--- helloworld/src/main/java/org/google/helloworld/DemoPersonApi.java
+++ helloworld/src/main/java/org/google/helloworld/DemoPersonApi.java
@@ -1,8 +1,11 @@
 package org.google.helloworld;
 
+import java.util.Properties;
+
 import org.google.helloworld.dao.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -10,7 +13,16 @@ public class DemoPersonApi {
   @Autowired private PersonRepository repository;
 
   public static void main(String[] args) {
-    SpringApplication.run(DemoPersonApi.class, args);
+    Properties props = new Properties();
+    props.put("spring.data.mongodb.host", System.getenv("MONGODB_HOST"));
+    props.put("spring.data.mongodb.port", System.getenv("MONGODB_PORT"));
+    props.put("spring.data.mongodb.username",
+        System.getenv("MONGODB_USER"));
+    props.put("spring.data.mongodb.password",
+        System.getenv("MONGODB_PASSWORD"));
+
+    new SpringApplicationBuilder(DemoPersonApi.class)
+        .properties(props).run(args);
   }
 }
 //  public void run(String... args) throws Exception {
