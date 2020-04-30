ced5a83fe4c808f6bdbd85f8636c98406c455d35
Tariq Yusuf <yusuft@google.com>
2020-04-30 11:18:09 -0800

    Change configuration to use kubernetes service

--- helloworld/src/main/java/org/google/helloworld/DemoPersonApi.java
+++ helloworld/src/main/java/org/google/helloworld/DemoPersonApi.java
@@ -4,18 +4,10 @@ import org.google.helloworld.dao.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.MongoDbFactory;
 
 @SpringBootApplication
 public class DemoPersonApi {
-
-  private final MongoDbFactory mongo;
-
   @Autowired private PersonRepository repository;
-
-  public DemoPersonApi(MongoDbFactory mongo) {
-    this.mongo = mongo;
-  }
 
   public static void main(String[] args) {
     SpringApplication.run(DemoPersonApi.class, args);
