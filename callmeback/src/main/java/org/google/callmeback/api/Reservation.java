5ec538652866ca6cf6079e000cf58bcc4acfd06a
Marc Jacobs <github@marcja.com>
2020-05-01 08:52:13 -0400

    Remove redundant data annotations

--- callmeback/src/main/java/org/google/callmeback/api/Reservation.java
+++ callmeback/src/main/java/org/google/callmeback/api/Reservation.java
@@ -3,9 +3,10 @@ package org.google.callmeback.api;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "reservations")
+@Document
 public class Reservation {
-  @Id public String id;
+  @Id 
+  public String id;
   public String name;
   public String phone;
   public String topic;
