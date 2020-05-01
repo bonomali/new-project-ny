d2ec00c371f234df132f23c07c83e7b752b4971d
Michelle Scharfstein <mscharfstein@google.com>
2020-05-01 16:19:03 -0400

    Change routing back to home page on another page

--- callmeback/frontend/src/components/ThankYou.jsx
+++ callmeback/frontend/src/components/ThankYou.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react'
+import { Link } from 'react-router-dom';
 
 function ThankYou() {
     return (
         <Container text style={{"paddingTop":"2em"}}>
             <div>Thank you!</div>
             <br/>
-            <div><a href="/">Go to NYS call homepage</a></div>
+            <div><Link to="/home">Go to NYS call homepage</Link></div>
         </Container>
     )
 }
