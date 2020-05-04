00e4cc76deb1546879bcfab9fdb1766508e318f8
Marc Jacobs <github@marcja.com>
2020-05-04 13:51:27 -0400

    Display app chrome with NYS logo

---
+++ callmeback/frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+      <img src="/nygov-logo.png" alt="New York State logo"/>
+    </header>
+  );
+}
+
+export default Header;
