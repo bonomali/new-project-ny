32574851ab5c537c1aa9dadf521dda530a3210a5
Ethan Gracer <egracer@google.com>
2020-05-07 19:52:22 +0000

    Removed extraneous attribute

--- helloworld/src/test/java/org/google/helloworld/DemoPersonApiTests.java
+++ helloworld/src/test/java/org/google/helloworld/DemoPersonApiTests.java
@@ -8,19 +8,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.google.helloworld.dao.FakePersonDataAccessService;
 import org.google.helloworld.model.Person;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-// Enable Spring support for JUnit
-@ExtendWith(SpringExtension.class) 
 // Start a test environment mimicking the @SpringBootApplication annotated class
 @SpringBootTest 
 // Create a MockMvc class and inject it into SpringBoot's dependency management system
