2a0c724d8bc34234e68ce99f4fa69326fe594db1
Kurt Ebrahim <kebrahim@google.com>
2020-05-13 16:22:30 -0400

    cleanup whitespace

--- callmeback/src/test/java/org/google/callmeback/CallMeBackControllerTest.java
+++ callmeback/src/test/java/org/google/callmeback/CallMeBackControllerTest.java
@@ -1,5 +1,6 @@
 package org.google.callmeback;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
@@ -8,54 +9,47 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class CallMeBackControllerTest {
 
-	@Autowired
-	private MockMvc mvc;
-
-	@Test
-	public void getHome() throws Exception {
-		mvc.perform(
-            MockMvcRequestBuilders.get("/home").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk());
-    }
-    
-    @Test
-	public void getThankYou() throws Exception {
-		mvc.perform(
-            MockMvcRequestBuilders.get("/thankyou").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk());
-    }
-    
-    @Test
-	public void getCancel() throws Exception {
-		mvc.perform(
-            MockMvcRequestBuilders.get("/cancel").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk());
-	}
+    @Autowired
+    private MockMvc mvc;
 
     @Test
-	public void getCancelConfirmation() throws Exception {
-		mvc.perform(
-            MockMvcRequestBuilders.get("/cancelconfirmation").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk());
+    public void getHome() throws Exception {
+        mvc.perform(get("/home").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
     }
-    
-    @Test
-	public void getReservations() throws Exception {
-		mvc.perform(
-            MockMvcRequestBuilders.get("/reservations/api/v1/123").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk());
-	}
 
-	@Test
-	public void getUnsupportedPathReturnsNotFound() throws Exception {
-		mvc.perform(
-            MockMvcRequestBuilders.get("/hithere").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isNotFound());
+    @Test
+    public void getThankYou() throws Exception {
+        mvc.perform(get("/thankyou").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getCancel() throws Exception {
+        mvc.perform(get("/cancel").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getCancelConfirmation() throws Exception {
+        mvc.perform(get("/cancelconfirmation").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getReservations() throws Exception {
+        mvc.perform(get("/reservations/api/v1/123").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getUnsupportedPathReturnsNotFound() throws Exception {
+        mvc.perform(get("/hithere").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNotFound());
     }
 }
