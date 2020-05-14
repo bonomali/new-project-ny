b0c595a8ec2b17107d40d63f426270bc39f226a1
Tariq Yusuf <yusuft@google.com>
2020-05-14 05:57:00 -0800

    Attempt at migrating frontend, still need to fix button UI

--- helloworldredux/frontend/setupTests.js
+++ helloworldredux/frontend/setupTests.js
@@ -1,6 +1,5 @@
-// optional: configure or set up a testing framework before each test
-// if you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
-
-// used for __tests__/testing-library.js
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect'
+import '@testing-library/jest-dom/extend-expect';
