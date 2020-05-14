2787ea99dbf1b845c1f6cc0bba692d52fe8179ce
Kurt Ebrahim <kebrahim@google.com>
2020-05-14 12:32:39 -0400

    fixed indenting

--- callmeback/src/test/java/org/google/callmeback/api/ReservationRepositoryTest.java
+++ callmeback/src/test/java/org/google/callmeback/api/ReservationRepositoryTest.java
@@ -9,61 +9,61 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class ReservationRepositoryTest {
-    @Autowired ReservationRepository reservationRepository;
+  @Autowired ReservationRepository reservationRepository;
 
-    final String businessTopic = "Business";
-    final String unemploymentTopic = "Unemployment";
-    final String dmvTopic = "DMV";
+  final String businessTopic = "Business";
+  final String unemploymentTopic = "Unemployment";
+  final String dmvTopic = "DMV";
 
-    @BeforeEach
-    public void setUp() {
-        reservationRepository.deleteAll();
-    }
+  @BeforeEach
+  public void setUp() {
+      reservationRepository.deleteAll();
+  }
 
-    @Test
-    public void testSave_setsId() {
-        Reservation unsavedReservation = new Reservation();
-        Reservation reservation = reservationRepository.save(unsavedReservation);
+  @Test
+  public void testSave_setsId() {
+      Reservation unsavedReservation = new Reservation();
+      Reservation reservation = reservationRepository.save(unsavedReservation);
 
-        assertThat(reservation.id).isNotNull();
-        assertThat(reservationRepository.findAll())
-            .containsExactlyInAnyOrder(reservation);
-    }
+      assertThat(reservation.id).isNotNull();
+      assertThat(reservationRepository.findAll())
+          .containsExactlyInAnyOrder(reservation);
+  }
 
-    @Test
-    public void testFindByTopic_singleReservation() {
-        String businessTopic = "Business";
-        String unemploymentTopic = "Unemployment";
-        Reservation unsavedReservation = new Reservation();
-        unsavedReservation.topic = businessTopic;
-        Reservation reservation = reservationRepository.save(unsavedReservation);
+  @Test
+  public void testFindByTopic_singleReservation() {
+      String businessTopic = "Business";
+      String unemploymentTopic = "Unemployment";
+      Reservation unsavedReservation = new Reservation();
+      unsavedReservation.topic = businessTopic;
+      Reservation reservation = reservationRepository.save(unsavedReservation);
 
-        assertThat(reservation.id).isNotNull();
-        assertThat(reservation.topic).isEqualTo(businessTopic);
-        assertThat(reservationRepository.findByTopic(businessTopic))
-            .containsExactlyInAnyOrder(reservation);
-        assertThat(reservationRepository.findByTopic(unemploymentTopic))
-            .isEmpty();
-    }
+      assertThat(reservation.id).isNotNull();
+      assertThat(reservation.topic).isEqualTo(businessTopic);
+      assertThat(reservationRepository.findByTopic(businessTopic))
+          .containsExactlyInAnyOrder(reservation);
+      assertThat(reservationRepository.findByTopic(unemploymentTopic))
+          .isEmpty();
+  }
 
-    @Test
-    public void testFindByTopic_multipleReservations() {
-        Reservation reservation1 = new Reservation();
-        reservation1.topic = businessTopic;
-        reservation1 = reservationRepository.save(reservation1);
+  @Test
+  public void testFindByTopic_multipleReservations() {
+      Reservation reservation1 = new Reservation();
+      reservation1.topic = businessTopic;
+      reservation1 = reservationRepository.save(reservation1);
 
-        Reservation reservation2 = new Reservation();
-        reservation2.topic = businessTopic;
-        reservation2 = reservationRepository.save(reservation2);
+      Reservation reservation2 = new Reservation();
+      reservation2.topic = businessTopic;
+      reservation2 = reservationRepository.save(reservation2);
 
-        Reservation reservation3 = new Reservation();
-        reservation3.topic = unemploymentTopic;
-        reservation3 = reservationRepository.save(reservation3);
+      Reservation reservation3 = new Reservation();
+      reservation3.topic = unemploymentTopic;
+      reservation3 = reservationRepository.save(reservation3);
 
-        assertThat(reservationRepository.findByTopic(businessTopic))
-            .containsExactlyInAnyOrder(reservation1, reservation2);
-        assertThat(reservationRepository.findByTopic(unemploymentTopic))
-            .containsExactlyInAnyOrder(reservation3);
-        assertThat(reservationRepository.findByTopic(dmvTopic)).isEmpty();
-    }
+      assertThat(reservationRepository.findByTopic(businessTopic))
+          .containsExactlyInAnyOrder(reservation1, reservation2);
+      assertThat(reservationRepository.findByTopic(unemploymentTopic))
+          .containsExactlyInAnyOrder(reservation3);
+      assertThat(reservationRepository.findByTopic(dmvTopic)).isEmpty();
+  }
 }
