96c64966a67728b944661b7531b2766e25b8b1e6
Kurt Ebrahim <kebrahim@google.com>
2020-05-14 12:19:09 -0400

    cleanup

--- callmeback/src/test/java/org/google/callmeback/api/ReservationRepositoryTest.java
+++ callmeback/src/test/java/org/google/callmeback/api/ReservationRepositoryTest.java
@@ -2,10 +2,6 @@ package org.google.callmeback.api;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
-
-import org.assertj.core.util.Arrays;
-import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
