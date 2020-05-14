771fe561c6a27cb3bb346ccc3e30cd745a6790c8
Michelle Scharfstein <mscharfstein@google.com>
2020-05-14 15:11:21 -0400

    Make suggested changes

--- callmeback/src/main/java/org/google/callmeback/api/ReservationEventType.java
+++ callmeback/src/main/java/org/google/callmeback/api/ReservationEventType.java
@@ -2,8 +2,6 @@ package org.google.callmeback.api;
 
 /** Represents a type of event in the reservation history. */
 public enum ReservationEventType {
-  /** Call as been removed from the queue by a call agent, who will attempt to reach the caller. */
-  INPROGRESS,
   /** Call was attempted but did not connect (busy, no answer, voicemail). */
   ATTEMPTED,
   /** Call connected and conversation started. */
