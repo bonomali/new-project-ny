987759db89275ff878bef9801e0d0ae13195751b
Kurt Ebrahim <kebrahim@google.com>
2020-05-15 17:11:02 -0400

    cleanup

--- callmeback/src/main/java/org/google/callmeback/api/QueueingRepository.java
+++ callmeback/src/main/java/org/google/callmeback/api/QueueingRepository.java
@@ -3,7 +3,6 @@ package org.google.callmeback.api;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.FindAndModifyOptions;
