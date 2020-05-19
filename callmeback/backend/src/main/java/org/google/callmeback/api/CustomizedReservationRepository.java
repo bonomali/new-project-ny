40e0dfe28b74ce720740851464d21f7c751aa8e2
Michelle Scharfstein <mscharfstein@google.com>
2020-05-19 16:01:25 -0400

    non working mess of a wait time calculation

--- callmeback/backend/src/main/java/org/google/callmeback/api/CustomizedReservationRepository.java
+++ callmeback/backend/src/main/java/org/google/callmeback/api/CustomizedReservationRepository.java
@@ -4,10 +4,21 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.Date;
 import java.util.Optional;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationExpression;
+import org.springframework.data.mongodb.core.aggregation.AggregationOperationContext;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.core.aggregation.GroupOperation;
+import org.springframework.data.mongodb.core.aggregation.ProjectionOperation;
+import org.springframework.data.mongodb.core.aggregation.ConditionalOperators.Cond;
 
 /**
  * A ReservationRepository that overrides various methods of MongoRepository.
@@ -61,10 +72,46 @@ class CustomizedReservationRepositoryImpl<T, ID> implements CustomizedReservatio
    */
   private ReservationWindow getWindow(Date requestDate) {
     ReservationWindow window = new ReservationWindow();
-    Query query =
+    Query eventsQuery =
         new Query(Criteria.where("events").is(null).and("reservationCreatedDate").lt(requestDate));
-    long countReservations = mongoTemplate.count(query, Reservation.class);
-    long expectedWaitTimeMins = countReservations * AVERAGE_WAIT_TIME_MINS;
+    long countReservations = mongoTemplate.count(eventsQuery, Reservation.class);
+  
+    /* db.rez.aggregate(
+      [{$set:
+        {connects:
+          {$arrayElemAt: [{$filter: {input: '$events', as: 'e', cond: {$eq: ['$$e.eventType', 'CONNECTED']}}}, 0]}
+        }
+      },
+      {$group:
+        {_id: 'ALL', avg_age: {$avg: {$subtract: ['$connects.eventDate', '$requestDate']}}}
+      }]).pretty()
+    */
+    /* filter example
+    new AggregationExpression() {
+                @Override
+                public DBObject toDbObject(AggregationOperationContext aggregationOperationContext) {
+                    DBObject filterExpression = new BasicDBObject();
+                    filterExpression.put("input", "$devices");
+                    filterExpression.put("as", "device");
+                    filterExpression.put("cond", new BasicDBObject("$eq", Arrays.<Object> asList("$$device.evaluationDate", date)));
+                    return new BasicDBObject("$filter", filterExpression);
+                }
+            }
+    */
+
+    ProjectionOperation projectStage =
+      Aggregation.project("requestDate").and("events").filter("e", Cond.when(Criteria.where("e.eventType")).equals("CONNECTED")).arrayElementAt(0).as("connected");
+    ProjectionOperation waitTime =
+      Aggregation.project().andExpression("connected.eventDate - requestDate").as("waitTime");
+    GroupOperation avgWait = Aggregation.group("id")            
+      .avg("waitTime").as("avgWait");
+            
+    Aggregation aggregation 
+      = Aggregation.newAggregation(projectStage, waitTime, avgWait);
+    
+    long avgWaitTime = mongoTemplate.aggregate(aggregation, "reservations");
+
+    long expectedWaitTimeMins = avgWaitTime - requestDate;
 
     Instant requestDateInstant = requestDate.toInstant();
     window.min =
