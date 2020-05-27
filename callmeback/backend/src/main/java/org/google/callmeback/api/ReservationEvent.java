9bc1267dbcc62266e0073239ab4834751c3d4c8f
Ethan Gracer <egracer@users.noreply.github.com>
2020-05-27 16:34:48 -0400

    Calculate exponential moving average (#161)
    When a new call is started, the API uses MongoDB aggregation to find the most recently stored moving average, and calculates the new moving average based on the new call's wait time (the difference between the reservation's requestDate and now).
    
    Tests will be included in a separate PR, so that other team members can be unblocked in the interim.

--- callmeback/backend/src/main/java/org/google/callmeback/api/ReservationEvent.java
+++ callmeback/backend/src/main/java/org/google/callmeback/api/ReservationEvent.java
@@ -9,4 +9,25 @@ public class ReservationEvent {
 
   /** The event type. */
   public ReservationEventType type;
+
+  /**
+   * Create an empty reservation event.
+   */
+  public ReservationEvent() {}
+
+  /**
+   * Create a reservation event.
+   * 
+   * @param date the timestamp indicating when the event occured.
+   * @param type the type of event that occurred.
+   */
+  public ReservationEvent(Date date, ReservationEventType type) {
+    this.date = date;
+    this.type = type;
+  }
+
+  /** Returns a ReservationEvent of ReservationEventType CONNECTED at the current Date. */
+  public static ReservationEvent newConnectedEvent() {
+    return new ReservationEvent(new Date(), ReservationEventType.CONNECTED);
+  }
 }
