4ae2544c6a905254ce1436ad4b80a46f44f42a34
Michelle Scharfstein <mscharfstein@google.com>
2020-05-27 11:15:42 -0400

    finish cancel test

--- callmeback/frontend/src/components/Cancel.test.js
+++ callmeback/frontend/src/components/Cancel.test.js
@@ -4,33 +4,39 @@ import { waitFor } from '@testing-library/dom';
 import '@testing-library/jest-dom/extend-expect'
 import axiosMock from 'axios'
 import Cancel from './Cancel';
+import { BrowserRouter as Router } from 'react-router-dom'
+import routeData from 'react-router';
 
 jest.mock('axios')
 let historyAdd = '';
 jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useHistory: () => ({
-    push: jest.fn((route)=>{historyAdd=route})
-  })
+    ...jest.requireActual('react-router-dom'),
+    useHistory: () => ({
+        push: jest.fn((route)=>{historyAdd=route})
+    }),
 }));
 
 test('cancels call', async () => {
-  render(<Cancel />);
+    const mockLocation = {
+        pathname: '/cancel',
+        state: { id: "id" },
+    }
+    jest.spyOn(routeData, 'useLocation').mockReturnValue(mockLocation)
+    render(
+        <Router>
+            <Cancel />
+        </Router>
+    );
 
-  axiosMock.patch.mockResolvedValueOnce({ data: {} })
+    axiosMock.patch.mockResolvedValueOnce({ data: {} })
 
-  const submit = screen.getByRole('button')
-  fireEvent.submit(submit)
+    const submit = screen.getByRole('button')
+    fireEvent.submit(submit)
 
-  expect(screen.getByRole('button')).toHaveBeenCalledTimes(1)
-
-  await waitFor(() => expect(axiosMock.patch).toHaveBeenCalledTimes(1))
-  expect(axiosMock.post).toHaveBeenCalledWith("/api/v1/reservations",
-  expect.objectContaining({
-    preferredName: 'mockname',
-    contactPhone: '1234567890',
-    query: 'mockquery',
-  }))
-  expect(historyAdd).toBe("/reservations/res-id");
-
+    await waitFor(() => expect(axiosMock.patch).toHaveBeenCalledTimes(1))
+    expect(axiosMock.patch).toHaveBeenCalledWith("/api/v1/reservations/id",
+        {resolution:
+            expect.objectContaining({type: 'CANCELED'})
+        })
+    expect(historyAdd).toBe("/cancelconfirmation");
 });
